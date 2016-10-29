8dde4edb156239c5e2ab9526fecd47677e84fa0e
shadypollen <coplayr@hotmail.com>
2016-10-29 21:47:24 +0100

    Added the weekly hours database insert function

--- hours_calculator.py
+++ hours_calculator.py
@@ -107,19 +107,23 @@ def week_calculator(days_hours_input):
 	week_hours = []
 	days_hours_list = list(days_hours_input)
 	for day in days_hours_list:
-		day_list = list(day)
-		pay_each_day = day_list[-2]
-		week_hours.append(pay_each_day)
+		week_hours.append(day)
+	sum_hours = sum(week_hours)
 	print(week_hours)
-	print(sum(week_hours))
-	hours_worked = int(sum(week_hours)/60)
-	minutes_worked = sum(week_hours)%60
-	print("Total amount of hours worked this week is: %d hours and %d minutes" % (hours_worked, minutes_worked))
-	return(hours_worked, minutes_worked, sum(week_hours))
+	print(sum_hours)
+	hours_worked = int(sum_hours/60)
+	minutes_worked = sum_hours%60
+	output_string = "%.2ih %.2im" % (hours_worked, minutes_worked)
+	print(output_string)
+
+	paid_total_pw = (8.2/60)* sum_hours
+	print(paid_total_pw)
+	return(output_string, paid_total_pw)
+
 
 
 def create_table():
-	cursor.execute("""CREATE TABLE IF NOT EXISTS overall_hours 
+	cursor.execute("""CREATE TABLE IF NOT EXISTS week_hours 
 		(no_of_week INTEGER, hours_worked_weekly TEXT, week_pay_estimate REAL)""")
 	cursor.execute("""CREATE TABLE IF NOT EXISTS day_hours 
 		(no_of_week INTEGER, what_day TEXT, full_date TEXT, start_fin_hours TEXT, hours_worked_daily TEXT, daily_pay_estimate REAL)""")
@@ -133,22 +137,39 @@ def daily_sql_insert(day_hours_counter):
 			(no_of_week, what_day, full_date, start_fin_hours, hours_worked_daily, daily_pay_estimate) 
 			VALUES(?, ?, ?, ?, ?, ?)""", (no_of_week, day_of_week, date, start_fin_hours, paid_hours, estimated_pay))
 		conn.commit()
-	cursor.close()
-	conn.close()
+
+def weekly_sql_insert(week_number, hours_pw, paid_pw):
+	cursor.execute("""INSERT INTO week_hours
+		(no_of_week, hours_worked_weekly, week_pay_estimate)
+		VALUES(?, ?, ?)""", (week_number, hours_pw, paid_pw))
+	conn.commit()
 		
 #Brings functions together
-def week_organiser():
+def var_collector():
 	day_hours_counter = []
+	weekly_hours_counter = []
+	global week_number
+	create_table()
 	while True:
 		user_input = input("Press ENTER to input a day or type 'End' if you'd like to finish:  \n")
 		if user_input.lower() == "end":
+			#These are the vars needed for the weekly table
+
+			hours_pw, paid_pw = week_calculator(weekly_hours_counter)
+			daily_sql_insert(day_hours_counter)
+			weekly_sql_insert(week_number, hours_pw, paid_pw)
+			cursor.close()
+			conn.close()
 			break
 		else:
+			#These are the vars needed for the daily table
+
 			#outputs the day of the week
 			day_input = day_list()
 			#outputs a string of the month's day
 			day_date, month_date, year_date = day_of_the_year()
 			date_string = str("%d-%d-%d" % (day_date, month_date, year_date))
+
 			week_number = which_week(year_date, month_date, day_date)
 			#inputs for beginning and end of shift
 			start_hours, start_minutes = start_hours_list()
@@ -161,23 +182,20 @@ def week_organiser():
 			#estimates the pay
 			estimated_pay = estimate_pay(paid_total)
 			#appends all the values for each day as a tuple in a list
-			day_hours_counter.append((week_number, day_input, date_string, start_fin_hours_return, hours_output_string, estimated_pay))
+			day_hours_counter.append((week_number, day_input, date_string, 
+				start_fin_hours_return, hours_output_string, estimated_pay))
+			weekly_hours_counter.append(paid_total)
 	#displays the total hours worked per week
-	create_table()
-	daily_sql_insert(day_hours_counter)
+	
+	
 	#creates the table and writes inputs in it
-
-
-def sql_values_input():
-	pass
-
 
 
 
 
 
 def main():
-	week_organiser()
+	var_collector()
 
 
 main()
