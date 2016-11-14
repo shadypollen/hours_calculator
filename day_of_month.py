ade9f77cacb69a07dc4b7ba445c6b264dcb488af
shadypollen <coplayr@hotmail.com>
2016-11-14 06:28:48 +0000

    Made an acceptable prototype for finding the work day in last/current week

--- day_of_month.py
+++ day_of_month.py
@@ -4,9 +4,9 @@ import calendar
 now = datetime.datetime.now()
 cal = calendar.Calendar()
 this_year = now.year
-this_month = now.month 
+this_month = 12#now.month 
 month_list = cal.monthdays2calendar(now.year, this_month)
-current_day_of_month = 7#now.day
+current_day_of_month = 5#now.day
 
 def day_list():
 	#Returns the day of the week you've worked and it's ordinal number
@@ -30,94 +30,48 @@ def day_list():
 
 	return (day_var, day_dict[day_var])
 
-def day_of_the_year_now():
+def find_week_index_by_day_m(month_list, search_day):
 	#Finds index of this week and the day of the week
-	for i in month_list:
-		for j in i:
-			day_of_month, day_of_week = j
-			if day_of_month == current_day_of_month: 
-				current_week_index = month_list.index(i)
+	for week in month_list:
+		for day in week:
+			day_of_month, day_of_week = day
+			if day_of_month == search_day: 
+				current_week_index = month_list.index(week)
 				current_day_of_week = day_of_week
-				print(current_week_index, current_day_of_week)
 				return(current_week_index, current_day_of_week)
 
-def work_week_index_finder(day_tuple):
-	current_week_index, current_day_of_week = day_tuple
-	is_it_last_month = False
+def find_day_w_in_last_month(month_list, search_day_w):
+	for day in month_list[-1]:
+		day_m, day_w = day 
+		if day_w == search_day_w:
+			return(day_m)
+
+def work_week_index_finder(current_week_index, current_day_of_week, day_ordinal_output):
 	if current_day_of_week == 0:
-		work_week_index = current_week_index - 1
-		if work_week_index <= 0:
-			is_it_last_month = True
-	else:
-		work_week_index = current_week_index
-	print(work_week_index, is_it_last_month)
-	return(work_week_index, is_it_last_month)
-
-def find_list_of_last_week(work_week_index, is_it_last_month, day_ordinal_output):
-	if is_it_last_month == True:
-		work_month = this_month - 1
-		if work_month <= 0:
-			work_month = 13 - this_month
-			work_year = this_year - 1
-		else:
-			work_year = this_year
-	else:
-		work_month = this_month
-		work_year = this_year
-	print(work_month, work_year)
-	for day in month_list[work_week_index]:
-		day_of_month, day_of_week = day 
-		if day_of_week == day_ordinal_output:
-			if day_of_month == 0:
-				work_month_print_output = work_month 
-				break
-			else:
-				work_month_print_output = work_month + 1
-				break
-	return(work_month, work_year, work_month_print_output)
-	
-def find_day_and_month_of_working(work_month, work_year, day_ordinal_output, is_it_last_month, work_week_index):
-	print("testtest", day_ordinal_output)
-	if is_it_last_month == True:
-		work_week_list = cal.monthdays2calendar(work_year, work_month)
-		print(work_week_list)
-		for week in reversed(work_week_list):
-			for day in reversed(week):
-				day_of_month, day_of_week = day
-				if day_of_week == day_ordinal_output:
-					if day_of_month == 0:
-						next_week_list = cal.monthdays2calendar(work_year, work_month+1)[0]
-						for day in next_week_list:
-							day_of_month, day_of_week = day 
-							if day_of_week == day_ordinal_output:
-								return(day_of_month)
-					else:
-						return(day_of_month)
-	else:
-		work_week_list = cal.monthdays2calendar(work_year, work_month)
-		for day in work_week_list[work_week_index]:
-			day_of_month, day_of_week = day 
-			if day_of_week == day_ordinal_output:
-				if day_of_month == 0:
-					next_week_list = cal.monthdays2calendar(work_year, work_month+1)[0]
-					for day in next_week_list:
-						day_of_month, day_of_week = day 
-						if day_of_week == day_ordinal_output:
-							return(day_of_month)
-				else:
-					return(day_of_month)
-	
-	
-
+		current_week_index -= 1
+	for day in month_list[current_week_index]:
+		day_m, day_w = day
+		if day_w == day_ordinal_output:
+			if day_m == 0:
+				last_month_list = cal.monthdays2calendar(now.year, this_month-1)
+				worked_day_m = find_day_w_in_last_month(last_month_list, day_ordinal_output)
+				return(this_month-1, worked_day_m)
+			elif day_m != 0:
+				return(this_month, day_m)
 
 		
 day_text_output, day_ordinal_output = day_list()
-print("test1", day_ordinal_output)
-today_week_output = day_of_the_year_now()
-work_week_index, is_it_last_month = work_week_index_finder(today_week_output)
-work_month, work_year, work_month_print_output = find_list_of_last_week(work_week_index, is_it_last_month, day_ordinal_output)
-worked_day_of_month = find_day_and_month_of_working(work_month, work_year, day_ordinal_output, is_it_last_month, work_week_index)
-print("worked day of month", worked_day_of_month, "worked month ouput:",work_month_print_output)
+current_week_index, current_day_of_week = find_week_index_by_day_m(month_list, current_day_of_month)
+work_month, work_day_m = work_week_index_finder(current_week_index,
+	 current_day_of_week, day_ordinal_output)
+print("Day worked: %d, month worked: %d" % (work_day_m, work_month))
+"""work_month, work_year, work_month_print_output = find_list_of_last_week(
+	work_week_index, is_it_last_month, day_ordinal_output)
+worked_day_of_month = find_day_and_month_of_working(work_month, 
+	work_year, day_ordinal_output, is_it_last_month, work_week_index)"""
+
+
+	#print("worked day of month", worked_day_of_month, "worked month ouput:",work_month_print_output)
 """Output worked_day_of_month, worked_month and worked_year"""
 
 #Figure out a way to properly write months
